4119c30218bb7b38b9df17670da23b44ad65941e
Dharambir Agrawal <dev.dharambir@gmail.com>
2025-04-12 16:24:32 +0000

    Added optimization in tictactoe game

--- Search/tictactoe/tictactoe.py
+++ Search/tictactoe/tictactoe.py
@@ -109,32 +109,96 @@ def utility(board):
     else:
         return 0
 
-def min_value(board):
+# def min_value(board):
+#     """
+#     Returns the minimum value of the board
+#     """
+#     v = math.inf
+
+#     # runs when game is over
+#     if terminal(board):
+#         return utility(board)
+    
+#     for action in actions(board):
+#         v = min(v, max_value(result(board,action)))
+#     return v
+
+# def max_value(board):
+#     """
+#     Returns the maximum value of the board
+#     """
+#     v = -math.inf
+
+#     # runs when game is over
+#     if terminal(board):
+#         return utility(board)
+    
+#     for action in actions(board):
+#         v = max(v, min_value(result(board,action)))
+#     return v
+
+# def minimax(board):
+#     """
+#     Returns the optimal action for the current player on the board.
+#     """
+#     # returns true if game is over
+#     if terminal(board): 
+#         return None
+    
+#     # X is max player
+#     elif player(board) == X:
+#         plays =[]
+
+#         for action in actions(board):
+#             plays.append([min_value(result(board,action)),action])
+
+#         # shorting and getting the maximum value
+#         return sorted(plays, key =lambda x:x[0], reverse= True)[0][1]
+#     # O is min player
+#     elif player(board) == O:
+#         plays =[]
+
+#         for action in actions(board):
+#             plays.append([max_value(result(board,action)),action])
+#         # shorting and getting the minimum value
+#         return sorted(plays, key =lambda x:x[0])[0][1]
+
+
+# optimized function with alpha beta pruning
+def min_value(board, alpha, beta):
     """
     Returns the minimum value of the board
     """
-    v = math.inf
 
     # runs when game is over
     if terminal(board):
         return utility(board)
     
+    v = math.inf
+    
     for action in actions(board):
-        v = min(v, max_value(result(board,action)))
+        v = min(v, max_value(result(board,action), alpha, beta))
+        if v <= alpha:
+            return v
+        beta = min(beta, v)
     return v
 
-def max_value(board):
+def max_value(board, alpha, beta):
     """
     Returns the maximum value of the board
     """
-    v = -math.inf
 
     # runs when game is over
     if terminal(board):
         return utility(board)
     
+    v = -math.inf
+    
     for action in actions(board):
-        v = max(v, min_value(result(board,action)))
+        v = max(v, min_value(result(board,action), alpha, beta))
+        if v >= beta:
+            return v
+        alpha = max(alpha, v)
     return v
 
 def minimax(board):
@@ -145,21 +209,35 @@ def minimax(board):
     if terminal(board): 
         return None
     
+    best_action =None
+    
     # X is max player
-    elif player(board) == X:
-        plays =[]
+    if player(board) == X:
+        
+        value = -math.inf
+        alpha = -math.inf
+        beta = math.inf
 
         for action in actions(board):
-            plays.append([min_value(result(board,action)),action])
 
-        # shorting and getting the maximum value
-        return sorted(plays, key =lambda x:x[0], reverse= True)[0][1]
+            min_result = min_value(result(board,action), alpha, beta)
+            if min_result > value:
+                value = min_result
+                best_action = action
+            alpha = max(alpha, value)
+
     # O is min player
     elif player(board) == O:
-        plays =[]
+        value = math.inf
+        alpha = -math.inf
+        beta = math.inf
 
         for action in actions(board):
-            plays.append([max_value(result(board,action)),action])
-        # shorting and getting the minimum value
-        return sorted(plays, key =lambda x:x[0])[0][1]
+            max_result = max_value(result(board, action), alpha, beta)
+            if max_result < value:
+                value = max_result
+                best_action = action
+            beta = min(beta, value)
+        
+    return best_action 
 
